4be5737bd3ee411916a2672ac97772661aae9bba
Renato Souza <rntsza@gmail.com>
2021-01-27 16:32:26 -0300

    Versão inicial

---
+++ ds-solar-calc/inv-power.py
@@ -0,0 +1,16 @@
+# import libs here
+import json
+
+
+def calc_power_by_inv(data=None):
+    # implement your code here
+    pass
+
+
+if __name__ == '__main__':
+    with open('D:\\OneDrive\\Trabalho\\Setor de desenvolvimento e soluções digitais\\project_git\\ea_interview\\data\\data.json') as json_file:
+        data = json.load(json_file)
+    print(data)
+    # call your function here
+
+    pass
